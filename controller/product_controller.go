56911dbcfb81df901a5027e6c1ad3fd53071a7fe
alexsandroocanha <alexsandroocanha@gmail.com>
2026-06-15 00:57:26 -0300

    chore: Create new usecase

--- controller/product_controller.go
+++ controller/product_controller.go
@@ -4,21 +4,32 @@ import (
 	"net/http"
 
 	"github.com/alexsandroocanha/Golang-Api-Crud/model"
+	"github.com/alexsandroocanha/Golang-Api-Crud/usecase"
 	"github.com/gin-gonic/gin"
 )
 
 type productController struct {
+	productUseCase usecase.ProductUsecase
 }
 
-func NewProductController() productController {
-	return productController{}
+func NewProductController(useCase usecase.ProductUsecase) productController {
+	return productController{
+		productUseCase: useCase,
+	}
 }
 
 func (p *productController) GetProducts(ctx *gin.Context) {
 	products := []model.Product{
-		ID:    1,
-		Name:  "Batata frita",
-		Price: 300,
+		{
+			ID:    1,
+			Name:  "Batata frita",
+			Price: 300,
+		},
+		{
+			ID:    2,
+			Name:  "Milk Shake",
+			Price: 250,
+		},
 	}
 
 	ctx.JSON(http.StatusOK, products)
--- main.go
+++ main.go
@@ -1,16 +1,26 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/alexsandroocanha/Golang-Api-Crud/controller"
+	"github.com/alexsandroocanha/Golang-Api-Crud/usecase"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
 	server := gin.Default()
+
+	ProductUseCase := usecase.NewProductUsecase()
+
+	productController := controller.NewProductController(ProductUseCase)
 
 	server.GET("/ping", func(ctx *gin.Context) {
 		ctx.JSON(200, gin.H{
 			"message": "pong",
 		})
 	})
+
+	server.GET("/products", productController.GetProducts)
 
 	server.Run(":8000")
 
